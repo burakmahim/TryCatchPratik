0fccaedbdd662b280c39875caafd36f705fa9aeb
burakmahim <burakmahim718@gmail.com>
2024-11-06 13:00:27 +0300

    Proje dosyası ekle.

---
+++ TryCatchPratik/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+    Console.Write("Lütfen bir sayı giriniz: ");
+    int sayi = int.Parse(Console.ReadLine());  // Kullanıcıdan sayı alma
+    int kare = sayi * sayi;
+    Console.WriteLine($"Girdiğiniz sayının karesi: {kare}");
+}
+catch (FormatException)
+{
+    // Geçersiz giriş durumunda hata mesajı
+    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+}
